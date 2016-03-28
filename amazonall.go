d4376600f8eaef1dac6ef229a61a61659de12b9b
charles <gaodihu@126.com>
2016-03-28 18:25:06 +0800

    fix bug

--- amazonall.go
+++ amazonall.go
@@ -3,7 +3,9 @@ package spider_lib
 // 基础包
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"net/url"
 	"os"
 	"regexp"
 	"strings"
@@ -11,20 +13,18 @@ import (
 	"github.com/PuerkitoBio/goquery"                        //DOM解析
 	"github.com/henrylee2cn/pholcus/app/downloader/request" //必需
 	. "github.com/henrylee2cn/pholcus/app/spider"           //必需
+	"github.com/henrylee2cn/pholcus/config"
 	. "github.com/henrylee2cn/pholcus/logs"
 	//. "github.com/henrylee2cn/pholcus/spider/common"          //选用
-	"github.com/henrylee2cn/pholcus/config"
 )
 
 func init() {
-	Amazon.Register()
+	AmazonAll.Register()
 }
 
-var Amazon = &Spider{
-	Name:        "Amazon",
-	Description: "Amazon商品数据 [Auto Page] [www.Amazon.com]",
-	// Pausetime: [2]uint{uint(3000), uint(1000)},
-	// Optional: &Optional{},
+var AmazonAll = &Spider{
+	Name:         "Amazon All",
+	Description:  "Amazon All商品数据 [Auto Page] [www.Amazon.com]",
 	EnableCookie: false,
 	RuleTree: &RuleTree{
 		Root: func(ctx *Context) {
@@ -56,73 +56,77 @@ var Amazon = &Spider{
 				}
 
 			}
+
 			/*
 				ctx.AddQueue(
 					&request.Request{
-						Url:  "http://www.amazon.com/IPOW/b/ref=bl_dp_s_web_8287399011?ie=UTF8&node=8287399011&field-lbr_brands_browse-bin=IPOW",
+						Url:  "http://www.amazon.com/gp/site-directory/ref=nav_shopall_btn",
 						Rule: "list",
 					},
 				)
-
-
-				ctx.AddQueue(
-					&request.Request{
-						Url:  "http://www.amazon.com/iWG-Dual-slot-Flashlight-Battery-Standard/dp/B016Y4RYU4/ref=sr_1_2?ie=UTF8&qid=1449729556&sr=8-2&keywords=myled",
-						Rule: "product",
-					},
-				)
 			*/
+
 		},
 
 		Trunk: map[string]*Rule{
 
 			"list": {
+				ItemFields: []string{
+					"pageurl",
+					"html",
+				},
 				ParseFunc: func(ctx *Context) {
 
 					query := ctx.GetDom()
-					//src, _ := query.Html()
+					pageurl := ctx.GetUrl()
 
-					if next_page := query.Find("#pagnNextLink"); next_page.Size() > 0 {
-						if next_url, ok := next_page.Attr("href"); ok {
-							if !strings.Contains(next_url, "www.amazon.com") {
-								next_url = "http://www.amazon.com" + next_url
-							}
-							ctx.AddQueue(&request.Request{
-								Url:  next_url,
-								Rule: "list",
-							},
-							)
+					is_product := false
+					if product_item := query.Find("[data-feature-name=productDetails]"); product_item.Size() > 0 {
+						is_product = true
+					}
+					if !is_product {
+						if strings.Contains(pageurl, "/dp/") {
+							is_product = true
 						}
 					}
+					if is_product {
 
-					lis := query.Find("li[data-asin]")
+						src, _ := query.Html()
 
-					lis.Each(func(i int, s *goquery.Selection) {
-						ASIN, _ := s.Attr("data-asin")
-						FBA := 0
-						if s.Find(".a-icon-prime").Size() != 0 {
-							FBA = 1
-						}
-						item := s.Find(".s-access-detail-page").Eq(0)
-						if url, ok := item.Attr("href"); ok {
-							tit, _ := item.Attr("title")
-							//fmt.Println(url)
-							//fmt.Println(tit)
-							ctx.AddQueue(
-								&request.Request{
-									Url:  url,
-									Rule: "product",
-									Temp: map[string]interface{}{
-										"ASIN":    ASIN,
-										"type":    tit,
-										"baseUrl": url,
-										"FBA":     FBA,
-									},
-								},
-							)
-						}
+						ctx.Output(map[int]interface{}{
+							0: pageurl,
+							1: src,
+						})
+						ctx.Parse("product")
+					} else {
+						query.Find("a").Each(func(i int, s *goquery.Selection) {
 
-					})
+							if next_url, ok := s.Attr("href"); ok {
+								url_arr, err := url.Parse(next_url)
+								fmt.Println("0:" + next_url)
+								if err == nil {
+									if strings.Contains(url_arr.Host, "amazon.com") {
+										ctx.AddQueue(&request.Request{
+											Url:  next_url,
+											Rule: "list",
+										},
+										)
+										fmt.Println("1:" + next_url)
+									}
+									if url_arr.Host == "" {
+										ctx.AddQueue(&request.Request{
+											Url:  "http://www.amazon.com" + next_url,
+											Rule: "list",
+										},
+										)
+										fmt.Println("2:" + next_url)
+									}
+								}
+							}
+
+						})
+					}
+
 				},
 			},
 
